cdacdd1cbdd3ed19963b5879f24e7f057298388b
Ammar <alharbiamar2@gmail.com>
2025-11-27 10:59:24 +0300

    Add non-breaking space after user greeting

--- user_page.php
+++ user_page.php
@@ -39,7 +39,7 @@ $projects = $conn->query("
     </a>
     <nav>
       <!---<div class="header-title">لهجتنا � المستخدم</div>--->
-      <span>مرحباً بك، <?= htmlspecialchars($user_name) ?></span>
+      <span>مرحباً بك، <?= htmlspecialchars($user_name) ?></span>&nbsp;
       <a href="user_answers.php" class="button">إجاباتي</a>
       <a href="logout.php">تسجيل الخروج</a>
     </nav>
