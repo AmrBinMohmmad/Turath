c549630320edaef41ef53f8d61304148b122f152
Albaraa Alkhathami <braalkhthmy@gmail.com>
2025-11-28 07:19:01 +0300

    Update admin_page.php
    adding dashboard
    still experimental

--- admin_page.php
+++ admin_page.php
@@ -49,7 +49,52 @@ $projects = $conn->query("SELECT * FROM cards ORDER BY id DESC");
 
     <!-- زر إنشاء اختبار تحت القائمة -->
     <a href="create_project.php" class="create-btn"> إنشاء اختبار جديد</a>
-  </div>
+  </div> <!--from here to 97 is changable-->
+      <h1 align="center">Projects Dashboard</h1>
+    <div class="overall">
+
+    </div>
+    <div class="Dashboard">
+        <form>
+            <div class="search">
+                <span class="material-symbols-outlined">search</span>
+                <input class="search_bar" type="search" name="search_bar" id="search_bar">
+            </div>
+        </form>
+        <table class="logs">
+            <tr class="tableHead">
+            <th>الإجابة</th>
+            <th>السؤال</th>
+            <th>المشروع</th>
+            <th>المستخدم</th>
+            </tr>
+            <tbody id="info">
+
+            </tbody>
+        </table>
+    </div>
+    <script>
+    var myArray = [
+	    {'name':'Michael', 'age':'30', 'birthdate':'11/10/1989'},
+	    {'name':'Mila', 'age':'32', 'birthdate':'10/1/1989'},
+	    {'name':'Paul', 'age':'29', 'birthdate':'10/14/1990'},
+	    {'name':'Dennis', 'age':'25', 'birthdate':'11/29/1993'},
+	    {'name':'Tim', 'age':'27', 'birthdate':'3/12/1991'},
+	    {'name':'Erik', 'age':'24', 'birthdate':'10/31/1995'},
+	]
+    function BuildTable(data) {
+        var table = document.getElementById('info')
+        for(var i = 0; i < data.length; i++) {  
+            var row = `<tr>
+                        <td>${data[i].name}</td>
+                        <td>${data[i].age}</td>
+                        <td>${data[i].birthdate}</td>
+                       </tr>`
+            table.innerHTML += row
+        }
+    }
+    BuildTable(myArray);
+</script>
 </main>
 
 <footer>
