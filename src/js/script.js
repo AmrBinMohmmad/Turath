7c141f6bd4965dd2a7521d2ee6ecb04e5a960eb3
AmrBinMohmmad <uglyz3tr@gmail.com>
2025-11-30 01:48:19 +0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,26 @@ function showForm(formId){
         .forEach(form => form.classList.remove("active"));
     document.getElementById(formId).classList.add("active");
 }
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+// حمّل الاختيار السابق إذا موجود
+if(localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-mode");
+    toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    
+    if(body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = "☀️";
+        localStorage.setItem("theme", "dark");
+    } else {
+        toggleBtn.textContent = "🌙";
+        localStorage.setItem("theme", "light");
+    }
+});
+
+
 
