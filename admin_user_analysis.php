e1212c48dff7288091e4d8839c63e0e74a433ffe
AmrBinMohmmad <uglyz3tr@gmail.com>
2025-12-07 01:47:17 +0300

    Add favicon to admin user analysis page
    Added favicon link to the HTML head section.

--- admin_user_analysis.php
+++ admin_user_analysis.php
@@ -85,6 +85,8 @@ $details = $conn->query($sql_details);
         href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;800&family=Outfit:wght@300;400;700&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="css/admin_user_analysis.css">
+     <link rel="icon" type="image/png" href="Favicon.png" />
+
 </head>
 <body>
 
@@ -223,4 +225,5 @@ $details = $conn->query($sql_details);
 </html>
 
 </html>
+
 
