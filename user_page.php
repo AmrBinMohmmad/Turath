17b442f385f5d0fe296df4d723cc46015c69fe3f
AmrBinMohmmad <uglyz3tr@gmail.com>
2025-12-07 01:41:04 +0300

    Update user_page.php

--- user_page.php
+++ user_page.php
@@ -75,7 +75,7 @@ $history = $conn->query($sql_history);
         href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;800&family=Outfit:wght@300;400;700&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="css/user_page.css">
-   <link rel="icon" type="image/png" href="favicon.png" />
+    <link rel="icon" type="image/png" href="Favicon.png" />
     
 </head>
 
