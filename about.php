3fdcc67ee6a671e8c65cc43b631efaf7bc150a05
AmrBinMohmmad <uglyz3tr@gmail.com>
2025-12-07 01:40:05 +0300

    Update about.php

--- about.php
+++ about.php
@@ -18,7 +18,7 @@ session_start(); ?>
       href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;800&family=Outfit:wght@300;400;700&display=swap"
       rel="stylesheet">
     <link rel="stylesheet" href="css/about_page.css">
-    <link rel="icon" type="image/png" href="favicon.png" />
+    <link rel="icon" type="image/png" href="Favicon.png" />
     
 </head>
 
